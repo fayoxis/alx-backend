0cf6015e93149136a58e709459e923bccf675e49
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-19 10:56:52 +0100

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
+"""Deletion-resilient hypermedia pagination with do-while loops
 """
 import csv
 from typing import Dict, List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """Server class to paginate a database.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
+        """Initialize a new Server instance.
         """
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Retrieve the cached dataset using a do-while loop.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -28,19 +28,21 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """Create an indexed dataset using a do-while loop.
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {}
+            i = 0
+            while i < len(dataset):
+                self.__indexed_dataset[i] = dataset[i]
+                i += 1
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Retrieves info about a page from a given index and with a
-        specified size.
+        """Retrieve info about a page from a given index and with a
+        specified size using a do-while loop.
         """
         data = self.indexed_dataset()
         assert index is not None and index >= 0 and index <= max(data.keys())
@@ -48,14 +50,14 @@ class Server:
         data_count = 0
         next_index = None
         start = index if index else 0
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
+        i = start
+        while i < len(data) and data_count < page_size:
+            item = data[i]
+            page_data.append(item)
+            data_count += 1
+            i += 1
+        if data_count == page_size:
+            next_index = i
         page_info = {
             'index': index,
             'next_index': next_index,
