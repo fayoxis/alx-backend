740da6fc3e04ffb4f1390f198974dd231199315d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-21 12:31:04 +0100

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python3
-"""Basic caching module.
+"""A basic caching module implemented using a dictionary.
 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary.
+    """A class that provides basic caching functionality.
+
+    Allows storing and retrieving key-value pairs in a dictionary.
     """
     def put(self, key, item):
-        """Adds an item in the cache.
+        """Store a key-value pair in the cache.
+
+        Args:
+            key (hashable):key associated with the value to be cached.
+            item (object): The value to be cached.
+
+        Returns:
+            None
         """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        do_put = True
+        while do_put:
+            if key is None or item is None:
+                do_put = False
+            else:
+                self.cache_data[key] = item
+                do_put = False
 
     def get(self, key):
-        """Retrieves an item by key.
+        """Retrieve a value from the cache using its key.
+
+        Args:
+            key (hashable): key associated with the value to be retrieved.
+
+        Returns:
+            object: value associated with the key, present in the cache.
+            None: If the key is not found in the cache.
         """
-        return self.cache_data.get(key, None)
+        do_get = True
+        result = None
+        while do_get:
+            result = self.cache_data.get(key, None)
+            do_get = False
+        return result
