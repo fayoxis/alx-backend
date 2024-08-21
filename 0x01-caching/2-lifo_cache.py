cf6f4de3ab8a8071f89f30c33a61f83e667cb7b0
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-21 12:46:45 +0100

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,42 +8,48 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Represents a LIFO (Last-In First-Out) cache.
-    This class inherits from BaseCaching, overrides put and get methods.
+    """This class represents a cache with a Least-In-First-Out
+    (LIFO) removal mechanism when the cache limit is reached.
     """
     def __init__(self):
-        """Initialize the cache.
-        Creates an empty OrderedDict to store key-value pairs.
+        """Initialize the cache with an empty OrderedDict.
         """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Add an item to the cache.
-        If the cache size exceeds the maximum allowed, discard the least
-        recently added item (LIFO) to make room for the new item.
 
-        Args:
-            key (str): The key associated with the item.
-            item (obj): The item to be cached.
+        If the key doesn't exist in the cache and adding the new
+        item would exceed the cache limit, remove the least
+        recently added items until there's enough space.
+        Then, add the new item to the cache.
+
+        If the key already exists, update its value and move it
+        to the end of the cache.
         """
         if key is None or item is None:
             return
 
         if key not in self.cache_data:
-            while len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(last=False)
+            do_discard = False
+            while len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
+                do_discard = True
 
-        self.cache_data[key] = item
+            if do_discard:
+                self.cache_data[key] = item
+
+        else:
+            self.cache_data[key] = item
+
         self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Retrieve an item from the cache.
-        Args:
-            key (str): The key associated with the item.
+        """Retrieve an item from the cache by its key.
 
-        Returns:
-            obj: The cached item if found, None otherwise.
+        Return the value of the item if the key exists,
+        otherwise return None.
         """
         return self.cache_data.get(key, None)
