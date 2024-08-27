b9cdd89911af1822362614fb2d14f14ea6142bd1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-27 18:30:15 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
-"""
+"""A simple Flask application."""
+
 from flask import Flask, render_template
 
 
+# Create a Flask instance
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+# Define a route for the root URL
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
+def index():
+    """Render the index template."""
     return render_template('0-index.html')
 
-
-if __name__ == '__main__':
+# Run the application if executed directly
+while __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
