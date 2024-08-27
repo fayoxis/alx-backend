c686416094b04f2aef591933996947b3be61c41e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-27 19:00:43 +0100

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 app.url_map.strict_slashes = False
