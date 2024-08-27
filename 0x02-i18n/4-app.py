7184919756b72312543b5ec047950fbf0a5c74ad
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-27 19:11:06 +0100

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-"""A Flask app with internationalization support."""
-
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
-# Flask Babel configuration
 class Config:
-    """Represents the configuration for Flask Babel."""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,35 +19,33 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-# Locale selector function
 @babel.localeselector
-def get_locale():
-    """Retrieves the locale for the web page.
-
-    Returns:
-        str: The selected locale.
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
     """
     queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    locale = app.config["BABEL_DEFAULT_LOCALE"]
+    query_table = {}
+    i = 0
+    if queries:
+        while True:
+            query = queries[i]
+            if '=' not in query:
+                query = '{}='.format(query)
+            key, value = query.split('=')
+            query_table[key] = value
+            i += 1
+            if i >= len(queries):
+                break
+
     if 'locale' in query_table:
-        while locale not in app.config["LANGUAGES"]:
-            locale = query_table['locale']
-    else:
-        locale = request.accept_languages.best_match(app.config["LANGUAGES"])
-    return locale
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-# Home page route
 @app.route('/')
-def get_index():
-    """Renders the home/index page.
-
-    Returns:
-        str: The rendered index page template.
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('4-index.html')
 
