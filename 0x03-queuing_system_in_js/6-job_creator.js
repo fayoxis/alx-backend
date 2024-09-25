bad042a7b0b7a0edd9af51b96cbdfc3db0fa569a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-25 16:26:00 +0100

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,19 +3,26 @@ import { createQueue } from 'kue';
 
 const queue = createQueue({name: 'push_notification_code'});
 
-const job = queue.create('push_notification_code', {
+const jobData = {
   phoneNumber: '4153518780',
-  message: 'This is the code to verify your account',
-});
+  message: 'This is the code to verify your account'
+};
+
+const job = queue.create('push_notification_code', jobData);
 
 job
   .on('enqueue', () => {
-    console.log('Notification job created:', job.id);
+    console.log(`Notification job created: ${job.id}`);
   })
   .on('complete', () => {
     console.log('Notification job completed');
   })
-  .on('failed attempt', () => {
+  .on('failed', () => {
     console.log('Notification job failed');
+  })
+  
+  .save((error) => {
+    if (error) {
+      console.log('Error creating job:', error);
+    }
   });
-job.save();
